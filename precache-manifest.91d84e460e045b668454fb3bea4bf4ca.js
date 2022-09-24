740c7babf57dc2a95b9eb556d591d04c8dc51d48
Hissue556 <hissue5.56@gmail.com>
2022-09-24 03:14:30 +0200

    Updates

---
+++ precache-manifest.91d84e460e045b668454fb3bea4bf4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "725e8f27226893217bea",
+    "url": "/fourthProject/static/css/main.e522ce8c.chunk.css"
+  },
+  {
+    "revision": "725e8f27226893217bea",
+    "url": "/fourthProject/static/js/main.725e8f27.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/fourthProject/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "6a9ed8bb838c5d13005f",
+    "url": "/fourthProject/static/js/2.6a9ed8bb.chunk.js"
+  },
+  {
+    "revision": "4c476ce4d749205b4950",
+    "url": "/fourthProject/static/js/runtime~main.4c476ce4.js"
+  },
+  {
+    "revision": "48fc5ce3b6e8641bac35ae01fd522627",
+    "url": "/fourthProject/index.html"
+  }
+];
